bc99dfdfb10819dea43d3cf9120660c3b9d95644
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2019-06-05 08:38:55 +0530

    tsv to json

--- run_classifier.py
+++ run_classifier.py
@@ -273,7 +273,7 @@ class MnliProcessor(DataProcessor):
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "dev_matched.tsv")),
+        self._read_json(os.path.join(data_dir, "dev_matched.tsv")),
         "dev_matched")
 
   def get_test_examples(self, data_dir):
