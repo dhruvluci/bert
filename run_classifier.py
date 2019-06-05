eee5210730276d8a940081731d0477a4ee13e9f3
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2019-06-05 06:34:24 +0530

    added qnli

--- run_classifier.py
+++ run_classifier.py
@@ -332,7 +332,39 @@ class MrpcProcessor(DataProcessor):
           InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
     return examples
 
+class QnliProcessor(DataProcessor):
+    """Processor for the QNLI data set (GLUE version)."""
 
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), 
+            "dev_matched")
+
+    def get_labels(self):
+        """See base class."""
+        return ["entailment", "not_entailment"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, line[0])
+            text_a = line[1]
+            text_b = line[2]
+            label = line[-1]
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+    
+    
 class ColaProcessor(DataProcessor):
   """Processor for the CoLA data set (GLUE version)."""
 
