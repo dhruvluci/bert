2d8c3678b8a1865f5397b73446caff93c8465eb4
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2019-06-05 19:33:11 +0530

    add test funct

--- run_classifier.py
+++ run_classifier.py
@@ -358,6 +358,12 @@ class QnliProcessor(DataProcessor):
         return self._create_examples(
             #self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev_matched")
             self._read_json(os.path.join(data_dir, "dev.json")), "dev_matched")
+    
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            #self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev_matched")
+            self._read_json(os.path.join(data_dir, "dev.json")), "dev_matched")
 
     def get_labels(self):
         """See base class."""
