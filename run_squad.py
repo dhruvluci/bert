7e02b646ea48ecd406c811e2915246ff3150a93b
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2019-06-27 23:16:44 +0530

    Update run_squad.py

--- run_squad.py
+++ run_squad.py
@@ -927,6 +927,20 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
     with tf.gfile.GFile(output_null_log_odds_file, "w") as writer:
       writer.write(json.dumps(scores_diff_json, indent=4) + "\n")
 
+def serving_input_receiver_fn():
+	feature_spec = {
+		"unique_ids": tf.FixedLenFeature([], tf.int64),
+		"input_ids": tf.FixedLenFeature([max_seq_length], tf.int64),
+		"input_mask": tf.FixedLenFeature([max_seq_length], tf.int64),
+		"segment_ids": tf.FixedLenFeature([max_seq_length], tf.int64),
+	}
+
+   	serialized_tf_example = tf.placeholder(dtype=tf.string,
+					       shape=[batch_size],
+					       name='input_example_tensor')
+	receiver_tensors = {'examples': serialized_tf_example}
+	features = tf.parse_example(serialized_tf_example, feature_spec)
+	return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
 
 def get_final_text(pred_text, orig_text, do_lower_case):
   """Project the tokenized prediction back to the original text."""
@@ -1182,11 +1196,21 @@ def main(_):
   # If TPU is not available, this will fall back to normal Estimator on CPU
   # or GPU.
   estimator = tf.contrib.tpu.TPUEstimator(
-      use_tpu=FLAGS.use_tpu,
+      use_tpu=False,
       model_fn=model_fn,
       config=run_config,
-      train_batch_size=FLAGS.train_batch_size,
-      predict_batch_size=FLAGS.predict_batch_size)
+      train_batch_size=6,
+      predict_batch_size=8)
+  estimator._export_to_tpu = False  ## !!important to add this
+  estimator.export_saved_model(
+        export_dir_base = EXPORT_PATH,
+        serving_input_receiver_fn = serving_input_receiver_fn)
+  #estimator = tf.contrib.tpu.TPUEstimator(
+      #use_tpu=FLAGS.use_tpu,
+      #model_fn=model_fn,
+      #config=run_config,
+      #train_batch_size=FLAGS.train_batch_size,
+      #predict_batch_size=FLAGS.predict_batch_size)
 
   if FLAGS.do_train:
     # We write to a temporary file to avoid storing very large constant tensors
