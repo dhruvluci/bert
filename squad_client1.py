c26c4cf368783e66b24d0213e9e650633624a08a
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2019-07-01 20:14:42 +0530

    Update squad_client1.py

--- squad_client1.py
+++ squad_client1.py
@@ -1406,8 +1406,8 @@ def get_qa(path):
 	for row in ri:
 		i+=1
 		final_result=process_output(row, 
-			   features[0][i], 
-			   features[1][i], 
+			   features[0], 
+			   features[1], 
 			   path, 5, 5, 30)
 		rx.append(final_result)
 	
