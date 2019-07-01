337c125630f153eeb57e4dd9f3af88c46733148a
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2019-07-01 21:20:18 +0530

    Update squad_client1.py

--- squad_client1.py
+++ squad_client1.py
@@ -1405,10 +1405,10 @@ def get_qa(path):
 	#for res in clean_result:
 		#final_result=write_predictions(features[0], features[1], res, 5, 30, False)
 		#rx.append(final_result)
-	#final_result=process_output(clean_result, 
-			   #features[0], 
-			   #features[1], 
-			   #path, 5, 5, 30)
+	final_result2=process_output(clean_result, 
+			   features[0], 
+			   features[1], 
+			   path, 5, 5, 30)
 	#for row in rs:
 		#i+=1
 		#final_result=process_output(row, 
@@ -1416,7 +1416,7 @@ def get_qa(path):
 			   #features[1], 
 			   #path, 5, 5, 30)
 		#rx.append(final_result)
-	return ri
+	return final_result2
 
 def get_qa2(stringx):
 	def process_inputs(input_data):
